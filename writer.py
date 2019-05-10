37ebcdbf2f17938377589f4db75fc03ca24e0679
Martin Schmidt <martin@scoding.dk>
2019-05-10 11:38:21 +0200

    Changed header in writer.py

--- writer.py
+++ writer.py
@@ -1,6 +1,6 @@
 
-	#Artifical load profile generator v1.3, generation of artificial load profiles to benchmark demand side management approaches
-    #Copyright (C) 2019 Martin Schmidt
+	#Artifical load profile generator v1.2, generation of artificial load profiles to benchmark demand side management approaches
+    #Copyright (C) 2018 Gerwin Hoogsteen
 
     #This program is free software: you can redistribute it and/or modify
     #it under the terms of the GNU General Public License as published by
